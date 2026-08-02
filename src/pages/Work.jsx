16314f28798d1b3e7ed0734118d8dbcc78d0c690
kik_renova_ai <kik87827@gmail.com>
2026-08-03 05:33:33 +0900

    목록 뿌리기 성공

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Work = () => {
+  const [work,setWork] = useState();
+  useEffect(()=>{
+    fetch('/data.json')
+    .then((result) => result.json())
+    .then((result) => {
+      setWork(result);
+    }).catch(error => console.log(error))
+  },[]);
   return (
     <section className="middle-wrap">
       <div className="middle-templete">
@@ -27,7 +35,32 @@ const Work = () => {
         <div className="middle-right-cols">
           <div className="middle-right-content">
             <div className="gallery-wrap">
-              <ul className="gallery-list"></ul>
+              <ul className="gallery-list">
+                {
+                  work && work.map((item,index) => {
+                    return (
+                      <li key={index}>
+                        <div className="gallery-item">
+                            <button className="gallery-thum-row" data-view = {item.detailHTML}>
+                              <img src={item.image} alt="" />
+                            </button>
+                            <div className="gallery-spec-row">
+                                <div className="gallery-spec-main-wrap">
+                                    <div className="gallery-spec-main">{item.specMain}</div>
+                                    <a href={item.specLink} target="_blank" title="새창열림" className="gallery-spec-link">
+                                      <img src="/images/open-new-window.svg" alt="" />
+                                    </a>
+                                </div>
+                                <div className="gallery-spec-sub-wrap">
+                                    <div className="gallery-spec-sub">{item.specSub}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    )
+                  })
+                }
+              </ul>
             </div>
           </div>
         </div>
